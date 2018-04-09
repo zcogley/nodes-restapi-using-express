3a6e6edfc7bfa57b2e813473ea68689eab040f89
Zac Cogley <zaccogley@gmail.com>
2018-04-09 15:46:06 -0500

    complete simple restapi to get and request data from a mysql server

---
+++ customer.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `customer` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `Country` varchar(100) NOT NULL,
+  `Phone` int(10) NOT NULL,
+  `Created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Updated_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
